a8fe016887e19fcdab2c669c07c97d6e6e0a5e77
sallixie <10191077@student.itk.ac.id>
2023-03-14 14:13:40 +0800

    add galeri beranda

--- assets/custom-script.js
+++ assets/custom-script.js
@@ -10,11 +10,46 @@ animateCard.forEach((card) => {
   });
 });
 
-const contactBtnSend = document.getElementById("contact-btn-send");
-contactBtnSend.addEventListener("click", (e) => {
-  Swal.fire({
-    icon: "success",
-    title: "Success",
-    text: "Your message has been sent!",
+try {
+  const contactBtnSend = document.getElementById("contact-btn-send");
+  contactBtnSend.addEventListener("click", (e) => {
+    Swal.fire({
+      icon: "success",
+      title: "Success",
+      text: "Your message has been sent!",
+    });
   });
-});
+} catch (error) {
+  //
+}
+
+try {
+  const galeriImgImg = document.getElementById("galeri-img-img");
+  const galeriImgModal = document.getElementById("galeri-img-modal");
+  const galeriImgModalClose = document.getElementById("galeri-img-modal-close");
+  const galeriImg = document.querySelectorAll(".galeri-img");
+  galeriImg.forEach((img) => {
+    img.addEventListener("click", (e) => {
+      galeriImgImg.src = img.src;
+      galeriImgModal.classList.add("show");
+
+      galeriImgModal.addEventListener("click", (e) => {
+        if (e.target.classList.contains("galeri-img-modal")) {
+          galeriImgModal.classList.remove("show");
+        }
+      });
+
+      document.addEventListener("keydown", (e) => {
+        if (e.key === "Escape") {
+          galeriImgModal.classList.remove("show");
+        }
+      });
+
+      galeriImgModalClose.addEventListener("click", (e) => {
+        galeriImgModal.classList.remove("show");
+      });
+    });
+  });
+} catch (error) {
+  //
+}
